f8b8b752cb47f32751714f3da43b55fa03997ce8
Sebastián Uribe <juansebastianuribe49@gmail.com>
2026-05-06 07:46:37 -0500

    feat(core-credentials): Definition of the recovery token.

--- src/core/credentials/mod.rs
+++ src/core/credentials/mod.rs
@@ -8,11 +8,13 @@ pub mod raw_credential;
 pub mod stored_credential;
 pub mod credential_status;
 pub mod credential_policy;
+pub mod recovery_token;
 
 pub use raw_credential::RawCredential;
 pub use stored_credential::StoredCredential;
 pub use credential_status::CredentialStatus;
 pub use credential_policy::CredentialPolicy;
+pub use recovery_token::RecoveryToken;
 
 #[cfg(test)]
 mod tests;
